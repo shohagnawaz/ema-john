7b0ce690e10bdd826fef64bcf1bfbc5a82be9f6a
Shohag Nawaz <snawazdigital@gmail.com>
2020-08-20 20:32:37 +0600

    Cart

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Cart = (props) => {
+    const cart = props.cart;
+    const totalPrice = cart.reduce((total, prd) => total + prd.price, 0)
+    return (
+        <div>
+            <h4>Order Summary</h4>
+            <p>Items Ordered: {cart.length}</p>
+        </div>
+    );
+};
+
+export default Cart;
